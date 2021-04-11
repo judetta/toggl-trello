8004b6bcfb2bccb6ae882a41e2264dbf3a78c2b6
Jutta Hänninen <75326735+judetta@users.noreply.github.com>
2021-04-11 20:10:46 +0300

    minor update to get trello cards in a dict

--- timeentries.py
+++ timeentries.py
@@ -1,4 +1,5 @@
 from togglapiutils import get_summary_report
+from trelloapiutils import list_ids, getTrelloCards
 from datetime import timedelta
 
 # Get summary report from Toggl and save projects and spent time in a dict
@@ -10,3 +11,14 @@ try:
             item['title']['project']: str(timedelta(milliseconds=item['time']))})
 except:
     print("Please check the request.")
+
+print(time_entries)
+
+# Get cards from trello
+for list_id in list_ids.values():
+    response = getTrelloCards(list_id)
+    # Put cards in a dict with name as key and card id as value
+    cards = {}
+    for card in response:
+        cards.update({card['name']: card['id']})
+    print(cards)
--- trelloapiutils.py
+++ trelloapiutils.py
@@ -1,12 +1,12 @@
 import requests
 
-
 # Possibly needed id's from Trello
+list_ids = {
+'courses_in_progress_list_id': '5f05c6d175bfc506e1166f54', 
+'waiting_for_credits_list_id': '5f3fbe38e514972615f51799',
+'finished_courses_list_id': '5f05c6d5ba81e62525d73f35'}
+
 time_spent_field_id = '603fc76549818d1e7bc152a3'
-board_id = '5f05c61c34d07b2234c58ec9'
-courses_in_progress_list_id = '5f05c6d175bfc506e1166f54'
-waiting_for_credits_list_id = '5f3fbe38e514972615f51799'
-finished_courses_list_id = '5f05c6d5ba81e62525d73f35'
 
 # Get Trello API key and token from files
 with open('trello-api-key.txt') as f1, open('trello-api-token.txt') as f2:
@@ -27,7 +27,7 @@ params = {
 
 
 def getTrelloCards(list_id):
-    """Get all the cards on Trello board"""
+    """Get the cards in a specific list on Trello by list id"""
     url = 'https://api.trello.com/1/lists/' + list_id + '/cards'
     response = requests.get(url, headers=headers, params=params)
     if response.status_code != 200:
@@ -35,8 +35,3 @@ def getTrelloCards(list_id):
     else:
         return response.json()
 
-cards = {}
-response = getTrelloCards(waiting_for_credits_list_id)
-for card in response:
-    cards.update({card['name']: card['id']})
-print(cards)
